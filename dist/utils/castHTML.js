29d89e465df5a0ba40b8fa48d7adf68a2990ffd6
MariusArhaug <mariusarhaug@hotmail.com>
2021-08-23 11:59:38 +0200

    CHANGE: remove dist

--- dist/utils/castHTML.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.castHTML = void 0;
-function castHTML(htmlArray) {
-    return htmlArray;
-}
-exports.castHTML = castHTML;
-//# sourceMappingURL=castHTML.js.map
